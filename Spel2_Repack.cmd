af45e253accbfc4fa633ea51f9ccb7a226e58a59
Merlinboss <75549907+MerlinbossDEV@users.noreply.github.com>
2020-12-06 12:58:37 +0100

    Spelunky 2 Soundbank Repack

--- Spel2_Repack.cmd
+++ Spel2_Repack.cmd
@@ -18,6 +18,11 @@ IF EXIST "Repack\FSB5\00000000.fsb" echo Warning, this will override the old rep
 
 API-Fmod\fsbankcl.exe -rebuild -format pcm %sfx-optimize% -o "Repack\FSB5\00000000.fsb" "Extracted\SFX\SFX.lst"
 
+REM Making sure that the new sfx .fsb file is not bigger than the extracted one
+FOR %%I in (Extracted\FSB5\00000000.fsb) do set size-sfx-ext=%%~zI
+FOR %%I in (Repack\FSB5\00000000.fsb) do set size-sfx-rep=%%~zI
+IF %size-sfx-rep% GTR %size-sfx-ext% echo. &echo THE NEW SFX .FSB FILE IS TOO BIG! &echo You need to reduce the size of your modified sfx files! &echo. &pause &exit
+
 :REPACK-MUSIC
 REM Use Fsbank to generate a new .fsb file from the music files list created during the extraction process
 REM The music files are compressed to vorbis, you can lower the quality value to make the music .fsb file smaller (71 will get it just under the original size)
@@ -26,6 +31,11 @@ IF NOT EXIST "Extracted\MUSIC\MUSIC.lst" echo The extracted MUSIC's .lst file is
 IF EXIST "Repack\FSB5\00000001.fsb" echo. &echo Warning, this will override the old repacked MUSIC! &timeout /T 10
 
 API-Fmod\fsbankcl.exe -rebuild -format vorbis -quality %music-compress% -o "Repack\FSB5\00000001.fsb" "Extracted\MUSIC\MUSIC.lst"
+
+REM Making sure that the new music .fsb file is not bigger than the extracted one
+FOR %%I in (Extracted\FSB5\00000001.fsb) do set size-music-ext=%%~zI
+FOR %%I in (Repack\FSB5\00000001.fsb) do set size-music-rep=%%~zI
+IF %size-music-rep% GTR %size-music-ext% echo. &echo THE NEW MUSIC .FSB FILE IS TOO BIG! &echo You need to reduce the size of your modified music files! &echo. &pause &exit
 
 :REPACK-SOUNDBANK
 REM Use QuickBMS to reinject the new .fsb files into the original soundbank
